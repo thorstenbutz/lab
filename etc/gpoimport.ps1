d22e37787523823eb3f6736d4fdf79ea92f32957
thorstenbutz <t@butz.io>
2024-04-17 14:49:58 +0200

    update

---
+++ etc/gpoimport.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Exported GPO: IT Lab Policy
+GpoId           : b0c3ef2e-cef6-44a8-bab2-b54c39154b0d
+Id              : 0f527411-9687-4ba7-994b-0b131b27ca3e
+#>
+
+Expand-Archive -Path '.\gpo.zip' -DestinationPath 'gpo' ## Creates new folder "gpo"
+New-GPO -Name 'IT Lab Policy'
+Import-GPO -Path C:\gpo\ -TargetName 'IT Lab Policy' -BackupGpoName 'IT Lab Policy'
+Get-GPO -Name 'IT Lab Policy' | New-GPLink -Target 'OU=it,DC=contoso,DC=com'
